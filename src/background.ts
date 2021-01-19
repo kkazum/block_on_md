007c5615689d0ee2b964e0e08b3c2baf3c81500f
kkazum <49956319+kkazum@users.noreply.github.com>
2021-01-19 23:38:10 +0900

    fix connection error

--- src/background.ts
+++ src/background.ts
@@ -16,26 +16,26 @@ langs.map(lang => {
 })
 
 const res = (response: any): void => {
-  try {
-    const {selectText, lang} = response;
+    const {selectText, lang}: { selectText: string, lang: string } = response;
     const textArea = document.createElement("textarea");
     document.body.appendChild(textArea);
     textArea.value = (lang == 'plain') ? `\`\`\`\n${selectText}\n\`\`\`\n` : `\`\`\`${lang}\n${selectText}\n\`\`\`\n`
     textArea.select();
     document.execCommand("copy");
     document.body.removeChild(textArea)
-  } catch (error) {
-    alert("Can't be allowed to use `ブロックを生成` on this page. If you're on github page, please reload!")
-  }
 }
 
 const insertLang = (info: chrome.contextMenus.OnClickData, tab: chrome.tabs.Tab): void => {
-  const { menuItemId } = info;
+  const { menuItemId }: { menuItemId: string } = info;
   chrome.tabs.sendMessage(tab.id!, {
     lang: menuItemId
   }, res);
 }
 
 chrome.contextMenus.onClicked.addListener((info, tab): void => {
-  insertLang(info, tab!);
+  if (tab!.url!.match(/github.com/)) {
+    insertLang(info, tab!);
+  } else {
+    alert("Can't be allowed to use `ブロックを生成` on this page. If you're on github page, please reload!")
+  }
 });
